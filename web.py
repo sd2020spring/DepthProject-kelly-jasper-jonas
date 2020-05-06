2afdfa891de59cb5e9312228eeffcc5856ac1a47
jellyyams <kelly.sy,yen@gmail.com>
2020-05-05 18:25:19 -0700

    changed private key

--- web.py
+++ web.py
@@ -466,7 +466,7 @@ if __name__ == '__main__':
         "type": "service_account",
         "project_id": os.environ.get('project_id'),
         "private_key_id": os.environ.get('private_key_id'),
-        "private_key": os.environ.get('private_key'),
+        "private_key": os.environ.get('private_key').replace('\\n', '\n'),
         "client_email": os.environ.get('client_email'),
         "client_id": os.environ.get('client_id'),
         "auth_uri": "https://accounts.google.com/o/oauth2/auth",
