d9cb6effe2ff0da26789c2493dab915a5929d05f
jellyyams <kelly.sy,yen@gmail.com>
2020-05-05 18:05:12 -0700

    dbug

--- web.py
+++ web.py
@@ -462,7 +462,17 @@ def edititem(itemid):
 if __name__ == '__main__':
     # Configures database and gets access to the database
     # cred = credentials.Certificate('ServiceAccountKey.json')
-    cred = credentials.Certificate(SECRET_KEY)
+    cred = credentials.Certificate({
+        "type": "service_account",
+        "project_id": os.environ.get('project_id),
+        "private_key_id": os.environ.get('private_key_id'),
+        "private_key": os.environ.get('private_key'),
+        "client_email": os.environ.get('client_email'),
+        "client_id": os.environ.get('client_id'),
+        "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+        "token_uri": "https://accounts.google.com/o/oauth2/token",
+        "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+        "client_x509_cert_url": os.environ.get('client_x509_cert_url')})
 
     firebase_admin.initialize_app(cred, {
     'storageBucket': 'depth-project-jkjkky.appspot.com'
